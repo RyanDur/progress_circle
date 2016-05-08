6c34fd02ad9d6e3b4d91827ed5f4d986760a72d1
RyanDur <ryan.p.durling@gmail.com>
2016-05-08 17:24:58 -0500

    simplify

--- common.js
+++ common.js
@@ -2,22 +2,12 @@
     var forEach = function (arr, func) {
         for (var i = 0; i < arr.length; i++) func(arr[i]);
     };
-    var resetChecked = function (elem) {
-        return function (e) {
-            if (!e.checked) elem(this).checked = false;
-        }
-    };
-    var on = function (event, func) {
-        return function (elem) {
-            elem.addEventListener(event, func)
-        };
-    };
-    var sibling = function (sibling) {
-        return function (elem) {
-            return elem.parentElement.querySelector(sibling)
-        }
-    };
 
     var checkboxes = document.querySelectorAll(".check");
-    forEach(checkboxes, on('change', resetChecked(sibling('.cmn-toggle'))));
+    forEach(checkboxes, function (elem) {
+        elem.addEventListener("change", function (e) {
+            if (!e.checked) elem.parentElement
+                .querySelector('.cmn-toggle').checked = false;
+        });
+    });
 })();
